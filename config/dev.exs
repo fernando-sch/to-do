d49f11b0c2f00f97e715240708cd6a5c8c35b1bb
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-01-18 18:03:31 -0300

    update dev and test database config

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :to_do, ToDo.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "to_do_dev",
+  username: System.get_env("DATABASE_USERNAME"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  hostname: System.get_env("DATABASE_HOST"),
+  database: System.get_env("DATABASE_NAME"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +19,7 @@ config :to_do, ToDo.Repo,
 config :to_do, ToDoWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
