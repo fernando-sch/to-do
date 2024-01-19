cc72284c47aa740aa759cf439bf528e2212fcc39
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-01-19 18:09:37 -0300

    add ex-machina lib to the project

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule ToDo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ex_machina, "~> 2.7.0", only: :test},
       {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
