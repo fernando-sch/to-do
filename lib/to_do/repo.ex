859b2ddbe17a3abc9f9392a54a4ac0369aa58d6b
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-01-29 20:22:50 -0300

    add cast id function

--- lib/to_do/repo.ex
+++ lib/to_do/repo.ex
@@ -2,4 +2,12 @@ defmodule ToDo.Repo do
   use Ecto.Repo,
     otp_app: :to_do,
     adapter: Ecto.Adapters.Postgres
+
+  @spec cast_id(any()) :: {:ok, Ecto.UUID.t()} | {:error, :invalid_id}
+  def cast_id(id) do
+    case Ecto.UUID.cast(id) do
+      {:ok, ^id} -> {:ok, id}
+      _ -> {:error, :invalid_id}
+    end
+  end
 end
